8d6cd32e4c9ed167f4195eac43e78035d5303d6c
sametcanvenedik <sametcanvenedik@gmail.com>
2022-02-14 18:52:34 +0300

    ..

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,3 +1,25 @@
-print("hello world")
-print("hello world")
-print("hello world")
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+tim1=0
+
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+
+    if success:
+        tim2 = time.time()
+        fps = str(int(1/(tim2-tim1)))
+        tim1 = tim2
+        cv2.putText(img, fps, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 1)
+        cv2.imshow("mycam",img)
+
+    cv2.waitKey(1)
+    
